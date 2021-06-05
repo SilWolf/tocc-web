1f909c04cbb36c14b4e02b8a35e316dcd71f7171
SilWolf <silwolf1121@gmail.com>
2021-06-05 11:26:57 +0800

    feat: login page ui

---
+++ pages/auth/login.tsx
@@ -0,0 +1,65 @@
+import { NextPage } from 'next'
+
+const LoginPage: NextPage = () => {
+	return (
+		<div
+			className='w-full h-screen bg-cover'
+			style={{ backgroundImage: 'url("/images/login-bg.jpg")' }}
+		>
+			<div className='container max-w-screen-md h-full flex items-center'>
+				<div className='w-full m-auto rounded-md bg-yellow-50 bg-opacity-90 shadow-lg px-8 py-6 space-y-6'>
+					<div className='space-y-6'>
+						<div>
+							<label
+								className='block text-sm text-gray-500 mb-1'
+								htmlFor='email'
+							>
+								Email
+							</label>
+							<input
+								type='email'
+								id='email'
+								className='form-input block w-full'
+							/>
+						</div>
+						<div>
+							<label
+								className='block text-sm text-gray-500 mb-1'
+								htmlFor='password'
+							>
+								密碼
+							</label>
+							<input
+								type='password'
+								id='password'
+								className='form-input block w-full'
+							/>
+						</div>
+					</div>
+
+					<div className='space-y-4 text-center'>
+						<button className='button button-primary w-full mx-auto active:ring-yellow-300'>
+							登入
+						</button>
+						<div className='w-full space-x-8 text-right'>
+							<a href='#' className='text-yellow-900 text-xs'>
+								註冊帳號
+							</a>
+							<a href='#' className='text-yellow-900 text-xs'>
+								無法登入
+							</a>
+						</div>
+					</div>
+
+					<hr />
+
+					<div>
+						<button className='button w-full mx-auto'>用Google登入</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default LoginPage
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,19 @@ module.exports = {
 	purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
+		container: {
+			center: true,
+			padding: {
+				DEFAULT: '4rem',
+			},
+		},
+		ripple: (theme) => ({
+			colors: theme('colors'),
+		}),
 		extend: {},
 	},
 	variants: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [require('@tailwindcss/forms')],
 }
