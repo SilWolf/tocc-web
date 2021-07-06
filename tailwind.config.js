499cb3c0a04338dbde5ae68901ec8b33cbe4bbbe
Dicky Chan <hinyiuchan@hk01.com>
2021-07-06 11:23:09 +0800

    chore: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@ module.exports = {
 	purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
+		screens: {
+			tablet: '640px',
+			// => @media (min-width: 640px) { ... }
+
+			laptop: '1024px',
+			// => @media (min-width: 1024px) { ... }
+		},
 		container: {
 			center: true,
 			padding: {
